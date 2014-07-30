03c66756d5e05c116aeebc43552e329335e1b7ea
Ryan Mawby <ryan@wegoodhq.com>
2014-07-30 13:22:48 +0100

    Initial file commit

---
+++ roku-remote.rb
@@ -0,0 +1,53 @@
+require 'io/console'
+
+# Reads keypresses from the user including 2 and 3 escape character sequences.
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
+end
+
+# oringal case statement from:
+# http://www.alecjacobson.com/weblog/?p=75
+def show_single_key
+  c = read_char
+
+  case c
+  when "\r"
+    do_key_press "Select"
+  when "\u007F"
+    do_key_press "Back"
+  when "\e[A"
+    do_key_press "up"
+  when "\e[B"
+    do_key_press "down"
+  when "\e[C"
+    do_key_press "right"
+  when "\e[D"
+    do_key_press "left"
+  when "\u0003"
+    puts "Bye!"
+    exit 0
+  when /^.$/
+    puts "SINGLE CHAR HIT: #{c.inspect}"
+  else
+    puts "SOMETHING ELSE: #{c.inspect}"
+  end
+end
+
+def do_key_press(key)
+  `curl -sd '' http://10.69.137.27:8060/keydown/#{key}`
+  `curl -sd '' http://10.69.137.27:8060/keyup/#{key}`
+end
+
+show_single_key while(true)
